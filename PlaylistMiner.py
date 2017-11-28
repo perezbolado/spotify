1ed5aa3ea8bfa195e293a953c9a951be3e1bab45
Amaury <perezbolado@gmail.com>
2017-11-28 00:30:30 -0500

    updated DAO with enrichment function, fixed bug on tracks with None id added a search method

--- PlaylistMiner.py
+++ PlaylistMiner.py
@@ -2,8 +2,17 @@ import SpotifyDAO
 import json
 
 dao = SpotifyDAO.SpotifyDAO()
-category = 'latin'
-playlists = dao.get_all_playlist_by_category(category)
+
+'''all playlists by category'''
+category_ids = dao.get_list_of_categories()
+for category in category_ids[7:]:
+    playlists = dao.get_all_playlist_by_category(category)
+    playlists = [dao.enrich_playlist(pl['owner']['id'], pl['id']) for pl in playlists]
+    with open(category + '.json', 'w') as outfile:
+        json.dump(playlists, outfile)
+
+'''Search for all playlists with character a'''
+playlists = dao.search('a', type='playlist', max_results=2050)
 playlists = [dao.enrich_playlist(pl['owner']['id'], pl['id']) for pl in playlists]
-with open(category + '.json', 'w') as outfile:
-    json.dump(playlists, outfile)
+with open('random_a.json', 'w') as outfile:
+        json.dump(playlists, outfile)
--- SpotifyDAO.py
+++ SpotifyDAO.py
@@ -1,8 +1,8 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-class SpotifyDAO:
 
+class SpotifyDAO:
     MAX_QUERY_RESULTS = 50
 
     def __init__(self):
@@ -27,7 +27,7 @@ class SpotifyDAO:
         playlist = self.sp.user_playlist(user_id, playlist_id)
         tracks = playlist['tracks']
         while tracks:
-            tracks_refs = tracks_refs + playlist['tracks']['items']
+            tracks_refs = tracks_refs + [tr for tr in playlist['tracks']['items'] if tr['track']['id'] is not None]
             if tracks['next']:
                 tracks = self.sp.next(tracks)
             else:
@@ -45,3 +45,35 @@ class SpotifyDAO:
             audio_features += self.sp.audio_features(tracks=track_ids[i:i + self.MAX_QUERY_RESULTS])
 
         return audio_features
+
+    def get_list_of_categories(self, country=None, locale=None):
+        cat_refs = [];
+        categories = self.sp.categories(country, locale, limit=self.MAX_QUERY_RESULTS)['categories']
+        while categories:
+            cat_refs += categories['items'];
+            if categories['next']:
+                categories = self.sp.next(categories)['categories']
+            else:
+                categories = None
+        return [ref['id'] for ref in cat_refs]
+
+    def search(self, q, type='playlist', market=None, max_results=None):
+        res_refs = []
+
+        type_map = {
+            'playlist': 'playlists',
+            'track': 'tracks',
+            'album': 'albums',
+            'artist': 'artists',
+        }
+
+        results = self.sp.search(q, self.MAX_QUERY_RESULTS, type=type, market=market)[type_map[type]]
+        while results:
+            res_refs += results['items']
+            if max_results is not None and len(res_refs) + self.MAX_QUERY_RESULTS > max_results:
+                break
+            if results['next']:
+                results = self.sp.next(results)[type_map[type]]
+            else:
+                results = None
+        return res_refs
